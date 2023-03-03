d6a085c3f70752b1011cd9b6c127f1fe704cfeb9
Daniel-Slavnyi <dslavnyj00@gmail.com>
2023-03-03 12:59:59 +0200

    Update apiNews.js

--- src/services/apiNews.js
+++ src/services/apiNews.js
@@ -1,11 +1,11 @@
 import { news } from './http';
 
-export const getNews = async () => {
+export const getNews = async (numOfPage = 1) => {
   const options = {
     q: ' Apple',
     sortBy: 'popularity',
     apiKey: '514c0f41e9e7409b90b4595e08780d51',
-    page: 1,
+    page: `${numOfPage}`,
     pageSize: 20,
   };
 
